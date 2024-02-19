07f25a4e3b88ceacbf6bacb960be6ae99e4f939f
Anna Chernova <annanvks@gmail.com>
2024-02-19 17:05:56 +0000

    deployed to gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/react-app-portfolio">
       <MyNavBar />
       <Routes>
         <Route path="/" element={<Home />} />
