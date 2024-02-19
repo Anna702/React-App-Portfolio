59e6c56108d017b4c5a6964f875cdaea8e355164
Anna Chernova <annanvks@gmail.com>
2024-02-19 16:46:56 +0000

    add basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { About, Home, Contacts, Projects } from "./pages";
 import MyNavBar from "./components/NavBar";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router basename="/">
       <MyNavBar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Projects />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </Router>
   );
 }
 
