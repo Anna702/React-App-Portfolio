d2eb16f7c5fa16c178cb85ee34e52734a531d475
Anna Chernova <annanvks@gmail.com>
2024-02-19 23:36:18 +0000

    fixed navbar to the top

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 
 function MyNavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary fixed-top">
       <Container>
         <Navbar.Brand href="#home">Anna Chernova</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
