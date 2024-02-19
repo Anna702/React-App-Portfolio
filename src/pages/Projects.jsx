af98a77f7ecb3cfb899274b35f42e0043d22913e
Anna Chernova <annanvks@gmail.com>
2024-02-19 16:47:19 +0000

    fixed cards appearance on the projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,14 @@
-import ProjectCard from "../components/ProjectCard";
+import CardProject from "../components/ProjectCard";
 import projects from "../projects.json";
 
 const Projects = () => {
   return (
     <>
       <h1>Projects Gallery</h1>
-      (projects.map(project => (
-      <ProjectCard key={project.id} project={project} />) ))
+
+      {projects.map((project) => (
+        <CardProject key={project.id} project={project} />
+      ))}
     </>
   );
 };
