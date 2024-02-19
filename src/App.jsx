94a583005a62d6c3e1cf86461cd2ea54192bb8a8
Anna Chernova <annanvks@gmail.com>
2024-02-19 17:27:21 +0000

    fixed basename path

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/react-app-portfolio">
+    <Router basename={`/react-app-portfolio`}>
       <MyNavBar />
       <Routes>
         <Route path="/" element={<Home />} />
