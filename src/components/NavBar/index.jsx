8cb236e9aff2248a2706906b56f0b147188c229e
Anna Chernova <annanvks@gmail.com>
2024-02-19 16:28:22 +0000

    changed main links in nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -11,24 +11,21 @@ function MyNavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">About me</Nav.Link>
-            <NavDropdown title="Projects" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About me</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contacts">Contacts</Nav.Link>
+            {/* <NavDropdown title="Projects" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Project 2
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Project 3</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
