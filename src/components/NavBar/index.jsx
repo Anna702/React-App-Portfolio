2dd4310d8b9402c1be78d23915bb3d75e797a7e2
Anna Chernova <annanvks@gmail.com>
2024-02-19 20:26:27 +0000

    fixed deploy paths bugs

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 function MyNavBar() {
   return (
@@ -11,10 +11,10 @@ function MyNavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About me</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/contacts">Contacts</Nav.Link>
+            <Nav.Link href="./">Home</Nav.Link>
+            <Nav.Link href="./about">About me</Nav.Link>
+            <Nav.Link href="./portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="./contacts">Contacts</Nav.Link>
             {/* <NavDropdown title="Projects" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
