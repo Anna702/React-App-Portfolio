4573beac1d366e176a5918444240562456e27e85
Anna Chernova <annanvks@gmail.com>
2024-02-19 23:37:31 +0000

    off commented basename, added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About, Home, Custom404, Contacts, Projects } from "./components/pages";
 import MyNavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <Router basename={`/React-App-Portfolio`}>
+    <Router>
       <MyNavBar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -14,8 +15,9 @@ function App() {
         <Route path="/404" element={<Custom404 />} />
         <Route path="/contacts" element={<Contacts />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
-
+// basename={`/React-App-Portfolio`}
 export default App;
