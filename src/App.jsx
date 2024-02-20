6f4e65b7458022c594794e5c33ce91b359b0a608
Anna Chernova <annanvks@gmail.com>
2024-02-20 00:26:55 +0000

    added page wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About, Home, Custom404, Contacts, Projects } from "./components/pages";
 import MyNavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import PageWrapper from "./components/PageWrapper";
 import "./App.css";
 
 function App() {
@@ -9,15 +10,50 @@ function App() {
     <Router>
       <MyNavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Projects />} />
-        <Route path="/404" element={<Custom404 />} />
-        <Route path="/contacts" element={<Contacts />} />
+        <Route
+          path="/"
+          element={
+            <PageWrapper>
+              <Home />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PageWrapper>
+              <About />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/portfolio"
+          element={
+            <PageWrapper>
+              <Projects />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/404"
+          element={
+            <PageWrapper>
+              <Custom404 />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PageWrapper>
+              <Contacts />
+            </PageWrapper>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
   );
 }
-// basename={`/React-App-Portfolio`}
+
 export default App;
