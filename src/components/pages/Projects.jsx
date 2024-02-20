563d39d57fa3b418ff76d0ebf4cb92fd20d95da7
Anna Chernova <annanvks@gmail.com>
2024-02-20 01:27:22 +0000

    added more colours to project cards

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,44 +3,76 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import projects from "../../projects.json";
 import { Button } from "react-bootstrap";
+import PageWrapper from "../PageWrapper";
 
 const Projects = () => {
   return (
     <>
-      <h1 className="text-center mb-4">Projects Gallery</h1>
-      <Row xs={1} sm={2} md={3} className="g-4">
-        {projects.map((project) => (
-          <Col key={project.id}>
-            <Card className="h-100">
-              <Card.Body>
-                <Card.Title>{project.title}</Card.Title>
-                <Card.Text>{project.pitch}</Card.Text>
-              </Card.Body>
-              <Card.Img
-                variant="top"
-                src={project.image}
-                style={{ height: "300px", objectFit: "cover" }}
-              />
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <Button
-                  variant="success"
-                  href={project.deployedLink}
-                  style={{ margin: "10px" }}
-                >
-                  Deployed app
-                </Button>
-                <Button
-                  variant="success"
-                  href={project.repoLink}
-                  style={{ margin: "10px" }}
-                >
-                  GitHub repo
-                </Button>
-              </div>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <PageWrapper>
+        <h1 className="text-center">Projects Gallery</h1>
+        <div className="text-center m-3">
+          <h5>
+            Explore my projects crafted with React, React Router, JSON, JSX,
+            JavaScript, HTML, CSS, and Bootstrap.
+          </h5>
+          <h5>
+            Click on the corresponding button to view each project online, and
+            access the GitHub repository for further exploration.
+          </h5>
+        </div>
+        <Row xs={1} sm={2} md={3} className="g-4">
+          {projects.map((project) => (
+            <Col key={project.id}>
+              <Card
+                className="h-100"
+                style={{ minWidth: "180px", border: "2px solid #547258" }}
+              >
+                <Card.Body>
+                  <Card.Title
+                    style={{
+                      padding: "10px",
+                      color: "white",
+                      backgroundColor: "#547258",
+                      borderRadius: "15px",
+                    }}
+                  >
+                    {project.title}
+                  </Card.Title>
+                  <Card.Text>{project.pitch}</Card.Text>
+                </Card.Body>
+
+                <Card.Img
+                  variant="top"
+                  src={project.image}
+                  style={{
+                    height: "300px",
+                    objectFit: "cover",
+                    borderTop: "2px solid #547258",
+                    borderBottom: "2px solid #547258",
+                  }}
+                />
+
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Button
+                    variant="success"
+                    href={project.deployedLink}
+                    style={{ margin: "10px", backgroundColor: "#547258" }}
+                  >
+                    Deployed app
+                  </Button>
+                  <Button
+                    variant="success"
+                    href={project.repoLink}
+                    style={{ margin: "10px", backgroundColor: "#547258" }}
+                  >
+                    GitHub repo
+                  </Button>
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </PageWrapper>
     </>
   );
 };
