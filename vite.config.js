df431d12da735f614c06ddb8e58dd60746432431
Anna702 <121174448+Anna702@users.noreply.github.com>
2024-02-20 11:19:23 +0000

    Pages (#1)
    * test
    
    * xx
    
    * vite
    
    * vite push path
    
    * fix
    
    * xx
    
    * browserrouter
    
    * build is back
    
    * 404
    
    * cleaning

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  build: {
-    outDir: "build",
-  },
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/React-App-Portfolio/";
+  }
+
+  return config;
 });
