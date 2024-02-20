58c3f39a3ce47e254671940ae7e272ac7be08f86
Anna Chernova <annanvks@gmail.com>
2024-02-20 00:10:34 +0000

    added header logo and colour to header

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,13 +1,23 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
 
 function MyNavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary fixed-top">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="fixed-top"
+      style={{ backgroundColor: "#ea6e3a" }}
+    >
       <Container>
-        <Navbar.Brand href="#home">Anna Chernova</Navbar.Brand>
+        <Navbar.Brand href="./">
+          <img
+            src="public/images/header logo pic.png"
+            alt="Anna Chernova logo"
+            style={{ maxHeight: "50px", marginRight: "10px" }}
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
@@ -15,17 +25,6 @@ function MyNavBar() {
             <Nav.Link href="./about">About me</Nav.Link>
             <Nav.Link href="./portfolio">Portfolio</Nav.Link>
             <Nav.Link href="./contacts">Contacts</Nav.Link>
-            {/* <NavDropdown title="Projects" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Project 2
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Project 3</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
